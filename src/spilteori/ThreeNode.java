179595c5eb882aed616064773736f454d218a6e7
deadshotcalamity <35096865+deadshotcalamity@users.noreply.github.com>
2018-04-23 15:05:29 +0200

    Add files via upload

--- src/spilteori/ThreeNode.java
+++ src/spilteori/ThreeNode.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spilteori;
 
-/**
- *
- * @author Stoxhorn
- */
 public class ThreeNode implements GameNode{
+    
+    int depth;
 
-    ThreeNode parent;
+    GameNode parent;
     
     Field field;
     
@@ -19,70 +12,105 @@ public class ThreeNode implements GameNode{
     
     int[] chances;
     
-    ThreeNode[] Children;
+    GameNode[] children;
     
-    ThreeNode[] BestNode;
+    GameNode[] bestNode;
     
-    public ThreeNode(Field newField, int newIndex, ThreeNode newParent)
+    //ThreeNode Object
+    public ThreeNode(Field newField, int newIndex, GameNode newParent)
     {
-        field = newField;
-        index = newIndex;
-        parent = newParent;
+        this.field = newField;
+        this.index = newIndex;
+        this.parent = newParent; 
+        children = null;        //Null is default and is changed when calling setChildren as we most likely wont know the children when creating the Node
     }
     
+    //getter for field
     @Override
     public Field getPosMove() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return field;
     }
 
+    //setter for field
     @Override
     public void setPosMove(Field newField) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        field = newField;
+        depth++;            //Updates the depth when a new move is made
     }
-
+    
+    //setter for chances
+    @Override
+    public void setChances(int[] newChances) {
+        chances = newChances;
+    }
+    
+    //getter for chances
     @Override
     public int[] getChances() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return chances;
     }
 
     @Override
     public void calculateChances() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //use minimax with winCheck from GameTree
+        //TODO
     }
-
+    
+    //Adds values to bestNode
     @Override
     public void calculateOptimal() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Initialize values
+        int player1Chance = 0;
+        int player2Chance = 0;
+        
+        int player1Index = 0;
+        int player2Index = 0;
+        
+        //Scans through all values from chances and stores the index and value of the best child for each player
+        for (int i = 0; i < chances.length; i++) {
+            int c = chances[i];
+            if (c > player1Chance) {
+                player1Chance = c;
+                player1Index = i;
+            }
+            if (c < player2Chance) {
+                player2Chance = c;
+                player2Index = i;
+            }
+        }
+        //The children with the best value for each player is put into bestNode
+        bestNode[0] = children[player1Index];
+        bestNode[1] = children[player2Index];
     }
-
+    
+    //getter for bestNode
     @Override
     public GameNode[] getOptimal() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return bestNode;
     }
-
+    
+    //getter for children
     @Override
     public GameNode[] getChildren() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return children;
     }
-
+    
+    //setter for children
     @Override
-    public void setChildren(GameNode[] ChildrenArray) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setChildren(GameNode[] childrenArray) {
+        children = childrenArray;
     }
-
+    
+    //getter for parent
     @Override
     public GameNode getParent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return parent;
     }
-
+    
+    //setter for parent
     @Override
-    public void setParent(GameNode NewGameNode) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setChances(int[] newChances) {
-        chances = newChances;
+    public void setParent(GameNode newGameNode) {
+        parent = newGameNode;
     }
     
 }
