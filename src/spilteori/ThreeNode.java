6fddd9dc38b59311417fadf5cc867809cbd8d399
Calamity <jscho17@student.sdu.dk>
2018-05-09 11:26:43 +0200

    added getPLayer()

--- src/spilteori/ThreeNode.java
+++ src/spilteori/ThreeNode.java
@@ -17,14 +17,14 @@ public class ThreeNode implements GameNode{
     
     private GameNode[] children;
     
-    //ThreeNode Object
+    //ThreeNode Object created through constructor
     public ThreeNode(Field newField, int newIndex, GameNode newParent)
     {
-        this.field = new ThreeField(newField);
-        this.index = newIndex;
-        this.depth = 0;
-        this.parent = newParent; 
-        children = null;        //Null is default and is changed when calling setChildren as we most likely wont know the children when creating the Node
+        this.field = new ThreeField(newField);  //Field given from constructor
+        this.index = newIndex;                  //Index given from constructor
+        this.depth = 0;                         //Default is 0
+        this.parent = newParent;                //Parent given from constructor
+        children = null;                        //Default is no children
     }
 
 
@@ -39,7 +39,7 @@ public class ThreeNode implements GameNode{
     @Override
     public void setPosMove(Field newField) {
         field = newField;
-        depth++;            //Updates the depth when a new move is made
+        depth++;
     }
     
     //setter for winValue
@@ -54,16 +54,59 @@ public class ThreeNode implements GameNode{
         return winValue;
     }
     
+    //getter for player at current depth
+    public int getPlayer() {
+        int tmp = (depth % 2) + 1;
+        return tmp;
+    }
+    
+    public GameNode getOptimalP1(int player) {
+        ArrayList<GameNode> childrenArray = new ArrayList<>();
+        
+        for (GameNode child : children) {
+            //Checks if the middle is a child and takes it as a move for player 1 as it is the optimal start. The AI wont pick it itself as it sees a draw no matter what from the starting position given that both players play optimally.
+            if( child.getField().getPos() == 4) {
+                return child;
+            }
+            //C
+            if (child.getWinValue()[0] == player) {
+                childrenArray.add(child);
+            }
+            else if (child.getWinValue()[0] == 0 && childrenArray.get(0).getWinValue()[0] == player){
+                
+            }
+        }
+    }
+    
+    private GameNode getOptimalP2() {
+        
+    }
+    
+    //Returns the child with the lowest depth. Should only be called on the winning value for a player.
+    private GameNode getBestChild(ArrayList<GameNode> childrenArray) {
+        GameNode bestChild = null;
+        for (GameNode child : childrenArray) {
+            //For the first for loop run
+            if (bestChild == null) {
+                bestChild = child;
+            }
+            else {
+                if (bestChild.getWinValue()[1] > child.getWinValue()[1]) {
+                    bestChild = child;
+                }
+            }
+        }
+        return bestChild;
+    }
+    
     @Override
     public GameNode getOptimal() {
-        
         ArrayList<GameNode> List1 = new ArrayList<>();
         ArrayList<GameNode> List2 = new ArrayList<>();
         ArrayList<GameNode> List3 = new ArrayList<>();
         
         if ((depth % 2) + 1 == 1) {
             for (GameNode child : children) {
-                //System.out.print(child.getWinValue()[0] + " - ");
                 if (child.getField().getPos() == 4) {
                     List1.add(child);
                 }
